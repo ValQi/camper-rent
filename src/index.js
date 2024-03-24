dde5d40df6baaa89d6e64a95823aa87a9bd19163
Vladyslav Teteruk <teteryk.vladislav@gmail.com>
2024-03-24 19:50:21 +0200

    sm-chang

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { Provider } from 'react-redux';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/campers-rent-rep">
+      <BrowserRouter basename="/camper-rent">
         <App />
       </BrowserRouter>
     </Provider>
